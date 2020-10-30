99ed143582d2c0acfdce9ee9b998103fb07de359
Guillaume CAMANES <guillaume.camanes@niji.fr>
2020-10-30 12:21:05 +0100

    # added firebase connection + classes

--- README.md
+++ README.md
@@ -2,15 +2,9 @@
 
 Flutter application to read manga scans
 
-## Getting Started
+## Firebase manager 
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+to run the python script :
+'''
+python3 ./python/firebase_manager.py
+'''
--- python/firebase_manager.py
+++ python/firebase_manager.py
@@ -1,4 +1,6 @@
+#-----------------------------------------------------------------------------------
 # SETUP
+#-----------------------------------------------------------------------------------
 # python3 -m pip install firebase-admin
 # python3 -m pip install google-cloud-firestore
 
@@ -6,13 +8,76 @@
 # then name it ServiceAccountKey.json
 # then place it in /python directory
 
+# IMPORT
+import os
+import sys
+import firebase_admin
+import google.cloud
+from firebase_admin import credentials, firestore
+
+#-----------------------------------------------------------------------------------
+# CONSTANTS
+#-----------------------------------------------------------------------------------
+
+# FIREBASE
+SERVICE_ACCOUNT_KEY_PATH = './python/ServiceAccountKey.json'
+
+# COLLECTION NAMES (CLOUD FIRESTORE)
+LIST_COLLECTION = u'mangasList'
+LIST_DOCUMENT = u'mangas'
+LIST_DOCUMENT_FIELD = u'list'
+
+# MANGA WEBSITE
+WEBSITE = 'http://www.mangapanda.com/'
+
+#-----------------------------------------------------------------------------------
+# FIREBASE MANAGER
+#-----------------------------------------------------------------------------------
+class FirebaseManager:
+    def __init__(self):
+        # Cloud Firestore certificate
+        self.cred = credentials.Certificate(SERVICE_ACCOUNT_KEY_PATH)
+        self.app = firebase_admin.initialize_app(self.cred)
+        # Get firestore client to interact with distant database
+        self.store = firestore.client()
+        self.mangaManager = MangaManager()
+
+    def getMangasList(self):
+        mangasList = []
+        try:
+            mangasList = self.store.collection(LIST_COLLECTION) \
+                .document(LIST_DOCUMENT) \
+                .get().to_dict()[LIST_DOCUMENT_FIELD]
+        except:
+            print('/!\ ERROR in getting manga list')
+            sys.exit()
+        return mangasList
+
+    def addManga(self, mangaUrlPart):
+        manga = self.mangaManager.getManga(mangaUrlPart)
+
+#-----------------------------------------------------------------------------------
+# MANGA MANAGER
+#-----------------------------------------------------------------------------------
+class MangaManager:
+    def __init__(self):
+        self.website = WEBSITE
+
+    def getManga(self, mangaUrlPart):
+        print('# GETTING INFO ON', mangaUrlPart, '...')
+
 #-----------------------------------------------------------------------------------
 # MAIN FUNCTION
 #-----------------------------------------------------------------------------------
 def main():
 
-    # Cloud Firestore certificate
-    cred = credentials.Certificate("./ServiceAccountKey.json")
-    app = firebase_admin.initialize_app(cred)
-    # Get firestore client to interact with distant database
-    store = firestore.client()
+    # Init FirebaseManager
+    firebaseManager = FirebaseManager()
+
+    mangasList = firebaseManager.getMangasList()
+    print('MANGA LIST', mangasList)
+
+    firebaseManager.addManga('one-piece')
+
+if __name__ == "__main__":
+    main()
