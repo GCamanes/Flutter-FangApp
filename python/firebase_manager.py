fd3aa60725148b525cc96bdcac26185c7254659b
Guillaume CAMANES <guillaume.camanes@niji.fr>
2020-10-30 13:31:33 +0100

    # added method to get manga infos

--- python/firebase_manager.py
+++ python/firebase_manager.py
@@ -19,8 +19,12 @@ from firebase_admin import credentials, firestore
 # CONSTANTS
 #-----------------------------------------------------------------------------------
 
+# FILES
+PATH = './python'
+MANGA_INFOS_PATH = '{}/mangaInfos.txt'.format(PATH)
+
 # FIREBASE
-SERVICE_ACCOUNT_KEY_PATH = './python/ServiceAccountKey.json'
+SERVICE_ACCOUNT_KEY_PATH = '{}/ServiceAccountKey.json'.format(PATH)
 
 # COLLECTION NAMES (CLOUD FIRESTORE)
 LIST_COLLECTION = u'mangasList'
@@ -29,6 +33,9 @@ LIST_DOCUMENT_FIELD = u'list'
 
 # MANGA WEBSITE
 WEBSITE = 'http://www.mangapanda.com/'
+MANGA_IMG_DIV = 'mangaimg'
+MANGA_PROPERTIES_DIV = 'mangaproperties'
+MANGA_PROPERTY_TD = 'propertytitle'
 
 #-----------------------------------------------------------------------------------
 # FIREBASE MANAGER
@@ -54,7 +61,11 @@ class FirebaseManager:
         return mangasList
 
     def addManga(self, mangaUrlPart):
-        manga = self.mangaManager.getManga(mangaUrlPart)
+        try:
+            mangaInfos = self.mangaManager.getMangaInfos(mangaUrlPart)
+            print('MANGA INFOS', mangaInfos)
+        except Exception as e:
+            print('# ERROR ADDING MANGA', mangaUrlPart, ':', str(e))
 
 #-----------------------------------------------------------------------------------
 # MANGA MANAGER
@@ -63,8 +74,58 @@ class MangaManager:
     def __init__(self):
         self.website = WEBSITE
 
-    def getManga(self, mangaUrlPart):
-        print('# GETTING INFO ON', mangaUrlPart, '...')
+    def getMangaInfos(self, mangaUrlPart):
+        url = '{}{}'.format(WEBSITE, mangaUrlPart)
+        print('# GETTING INFO ON {} at {} ...'.format(mangaUrlPart, url))
+
+        # Initialize manga getMangaInfos
+        mangaInfos = {
+            'name': '',
+            'imgUrl': '',
+            'url': url,
+            'status': '',
+            'author': '',
+            'released': '',
+            'lastChapter': 'None',
+        }
+
+        # Get html content in a file
+        os.system('curl -s {} +  > {}'.format(url, MANGA_INFOS_PATH))
+        # read the file
+        f = open(MANGA_INFOS_PATH, 'r')
+        content = f.readlines()
+        f.close()
+        os.system('rm {}'.format(MANGA_INFOS_PATH))
+
+        isImgSaved = False
+        inPropertiesDiv = False
+        nextIsProperty = False
+        properties = []
+        for line in content:
+            if (isImgSaved):
+                if (MANGA_PROPERTIES_DIV in line):
+                    inPropertiesDiv = True
+                    continue
+                if (inPropertiesDiv and MANGA_PROPERTY_TD in line):
+                    nextIsProperty = True
+                    continue
+                if (nextIsProperty and '<td>' in line and '</td>' in line and len(properties) < 4):
+                    properties.append(line.split('<td>')[1].split('</td>')[0])
+                    nextIsProperty = False
+                if ('</div>' in line):
+                    inPropertiesDiv = False
+                    break
+
+            elif (MANGA_IMG_DIV in line):
+                mangaInfos['imgUrl'] = line.split('src="')[1].split('"')[0]
+                isImgSaved = True
+
+        mangaInfos['name'] = properties[0]
+        mangaInfos['released'] = properties[1]
+        mangaInfos['status'] = properties[2]
+        mangaInfos['author'] = properties[3]
+
+        return mangaInfos
 
 #-----------------------------------------------------------------------------------
 # MAIN FUNCTION
