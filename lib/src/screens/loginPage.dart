84e975b78578e7c9c9461a0e459c8f21c794bad2
Guillaume CAMANES <guillaume.camanes@niji.fr>
2020-11-13 10:59:48 +0100

    # added images on login page

--- lib/src/screens/loginPage.dart
+++ lib/src/screens/loginPage.dart
@@ -12,32 +12,35 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.black,
+      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CustomTextField(
-                  onChangedValue: (String text) async {
-                    setState(() { _userName = text; });
-                  },
-                  icon: Icons.person,
-                  hintText: 'Username',
-                ),
-                CustomTextField(
-                  onChangedValue: (String text) async {
-                    setState(() { _password = text; });
-                  },
-                  icon: Icons.lock,
-                  hintText: 'Password',
-                  isObscure: true,
-                ),
-              ],
-            )
+          Padding(
+            padding: EdgeInsets.only(top: size.height * 0.35),
+            child: Center(
+              child: Column(
+                children: [
+                  CustomTextField(
+                    onChangedValue: (String text) async {
+                      setState(() { _userName = text; });
+                    },
+                    icon: Icons.person,
+                    hintText: 'Username',
+                  ),
+                  CustomTextField(
+                    onChangedValue: (String text) async {
+                      setState(() { _password = text; });
+                    },
+                    icon: Icons.lock,
+                    hintText: 'Password',
+                    isObscure: true,
+                  ),
+                ],
+              )
+            ),
           ),
           Positioned(
               bottom: 70,
@@ -46,6 +49,22 @@ class _LoginPageState extends State<LoginPage> {
               child: Center(
                   child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation(Colors.yellow), backgroundColor: Colors.black)
               )
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(
+              'lib/src/assets/login-cloud-top.png',
+              width: size.width * 1.1,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Image.asset(
+              'lib/src/assets/login-cloud-bottom.png',
+              width: size.width * 0.7,
+            ),
           )
         ],
       )
