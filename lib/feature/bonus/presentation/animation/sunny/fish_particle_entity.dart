acea9329d0a727d98909c1d0a22c4148e0d84371
gcamanes <guillaume.camanes@niji.fr>
2021-10-23 16:26:44 +0200

    animation: added fish image and particle

---
+++ lib/feature/bonus/presentation/animation/sunny/fish_particle_entity.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:fangapp/core/enum/animation_enum.dart';
+import 'package:flutter/animation.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+import '../particle_entity.dart';
+
+class FishParticleEntity extends ParticleEntity {
+  FishParticleEntity({
+    required Random random,
+  }) : super(random: random);
+
+  @override
+  void initTween() {
+    // Get random (x, y) offset for start and end of animation
+    final Offset startPosition = Offset(1.3, 0.9 - 0.2 * random.nextDouble());
+    final Offset endPosition = Offset(-0.3, 0.9 - 0.2 * random.nextDouble());
+
+    tween = TimelineTween<AnimationEnum>()
+      ..addScene(begin: Duration.zero, duration: animDuration).animate(
+        AnimationEnum.x,
+        tween: Tween<double>(begin: startPosition.dx, end: endPosition.dx),
+        curve: Curves.linear,
+      )
+      ..addScene(begin: Duration.zero, duration: animDuration).animate(
+        AnimationEnum.y,
+        tween: Tween<double>(begin: startPosition.dy, end: endPosition.dy),
+        curve: Curves.easeIn,
+      );
+  }
+}
