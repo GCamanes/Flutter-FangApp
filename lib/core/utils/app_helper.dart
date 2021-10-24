f4087b9f0abb750127f77af22e27376c4e0d8c6f
gcamanes <guillaume.camanes@niji.fr>
2021-10-24 15:59:28 +0200

    app: updated app helper to singleton

--- lib/core/utils/app_helper.dart
+++ lib/core/utils/app_helper.dart
@@ -1,8 +1,23 @@
 import 'dart:ui';
 
-class AppHelper {
-  static Size size = Size.zero;
+import 'package:flutter/material.dart';
+
+abstract class AppHelperBase {
+  @protected
+  late Size size;
 
   Size get deviceSize => size;
   set deviceSize(Size newSize) => size = newSize;
+}
+
+class AppHelper extends AppHelperBase{
+  factory AppHelper() {
+    return _instance;
+  }
+
+  AppHelper._internal() {
+    size = Size.zero;
+  }
+
+  static final AppHelper _instance = AppHelper._internal();
 }
